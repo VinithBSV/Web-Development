935136c0169bd78c034724b472956467a778fe06
VinithBSV <vinithbsv711@gmail.com>
2024-09-10 08:04:42 +0530

    Added Image Uploader file.

---
+++ Image uploader/script.js
@@ -0,0 +1,5 @@
+const img = document.getElementById("thumb");
+const input = document.getElementById("file");
+input.addEventListener("change", (e) => {
+    img.src = URL.createObjectURL(e.target.files[0]);
+})
